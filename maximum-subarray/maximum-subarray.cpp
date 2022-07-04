b9a5e520b503127410e773f4ad804f6c53d53c10
Abdeali Jaroli <56578305+abdealijaroli@users.noreply.github.com>
2022-07-04 12:51:16 +0530

    Time: 168 ms (45.27%), Space: 67.7 MB (53.51%) - LeetHub

--- maximum-subarray/maximum-subarray.cpp
+++ maximum-subarray/maximum-subarray.cpp
@@ -1,65 +1,16 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int maxSum = nums[0];
+        int sum = 0;
         
-        //kadane's 
-        
-        int currentSum = 0;
-        int largestSum = INT_MIN;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            currentSum += nums[i];
-            largestSum = max(largestSum, currentSum);
-            
-            if (currentSum < 0)
-            {
-                currentSum = 0;
-            }
+        for(auto it : nums){
+            sum += it;
+            maxSum = max(sum, maxSum);  
+            if(sum<0)
+                sum = 0;
         }
-        return largestSum;
-}
-        
-        
-        
-    //prefix sum approach
-//         int maxSum=INT_MIN;
-        
-//         if (nums.size() == 1){
-//             maxSum = nums[0];
-//         }
-        
-//         //prefix logic
-//         int prefix[nums.size()];
-//         prefix[0] = nums[0];
-//         for (int i = 1; i < nums.size(); i++){
-//             prefix[i] = prefix[i - 1] + nums[i];
-//         }
-        
-//         // looping
-//         for (int i=0; i<nums.size(); i++){
-//             for (int j=i; j<nums.size(); j++){
-//                  int currentSum = i > 0 ? prefix[j] - prefix[i - 1] : prefix[j];
-//                 maxSum = max(maxSum, currentSum);
-//             }
-//         }
-//         return maxSum;
-//     }
-        
-        
-        //bruteforce 
-    //     int maxSum=INT_MIN;
-    //     if (nums.size() == 1){
-    //         maxSum = nums[0];
-    //     }
-    //     for (int i=0; i<nums.size(); i++){
-    //         for (int j=i; j<nums.size(); j++){
-    //             int currentSum=0;
-    //             for (int k=i; k<=j; k++){
-    //                 currentSum+=nums[k];
-    //             }
-    //             maxSum = max(maxSum, currentSum);
-    //         }
-    //     }
-    //     return maxSum;
-    // }
+        return maxSum;
+    }
 };
