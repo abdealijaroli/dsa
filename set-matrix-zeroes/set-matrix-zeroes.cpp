8f90aad2d5fd6f0e399be5ab4039e04a33594616
Abdeali Jaroli <56578305+abdealijaroli@users.noreply.github.com>
2022-07-13 13:15:17 +0530

    Time: 7 ms (99.71%), Space: 13.2 MB (52.88%) - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.cpp
+++ set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -2,30 +2,72 @@ class Solution{
 public:
     void setZeroes(vector<vector<int>> &matrix)
     {
-        //bool rowZero = false;
+        int col0 = 1;
+        int rows = matrix.size();
+        int cols = matrix[0].size();
         
-        vector<bool> rowVec(matrix.size());
-        vector<bool> colVec(matrix[0].size());
-        
-        for (int r = 0; r < matrix.size(); r++)
-        {
-            for (int c = 0; c < matrix[0].size(); c++)
-            {
-                if (matrix[r][c] == 0){
-                    rowVec[r] = true;
-                    colVec[c] = true;
+        for(int i=0; i<rows; i++){
+            if(matrix[i][0] == 0)
+                col0 = 0;
+            
+            for(int j=1; j<cols; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
         
-        for (int r = 0; r < matrix.size(); r++)
-        {
-            for (int c = 0; c < matrix[0].size(); c++)
-            {
-                if (rowVec[r] || colVec[c]){
-                    matrix[r][c] = 0;                    
+        for(int i=rows-1; i>=0; i--){
+            for(int j=cols-1; j>=1; j--){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
                 }
             }
+        if(col0 == 0)
+            matrix[i][0] = 0;
         }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         //bool rowZero = false;
+        
+//         vector<bool> rowVec(matrix.size());
+//         vector<bool> colVec(matrix[0].size());
+        
+//         for (int r = 0; r < matrix.size(); r++)
+//         {
+//             for (int c = 0; c < matrix[0].size(); c++)
+//             {
+//                 if (matrix[r][c] == 0){
+//                     rowVec[r] = true;
+//                     colVec[c] = true;
+//                 }
+//             }
+//         }
+        
+//         for (int r = 0; r < matrix.size(); r++)
+//         {
+//             for (int c = 0; c < matrix[0].size(); c++)
+//             {
+//                 if (rowVec[r] || colVec[c]){
+//                     matrix[r][c] = 0;                    
+//                 }
+//             }
+//         }
     }
 };
